ca7a46534656ab497b4ca2201fe219eeb2d37d18
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-15 22:28:57 +0600

    payment successfully executed

--- src/Pages/Dashboards/StudentDashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboards/StudentDashboard/Payment/CheckoutForm.jsx
@@ -1,11 +1,25 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 // eslint-disable-next-line react/prop-types
-const CheckoutForm = ({ selectedClass }) => {
+const CheckoutForm = ({ selectedClass: { price, _id } }) => {
   const stripe = useStripe();
   const elements = useElements();
+  const { user } = useAuth();
+  const [axiosSecure] = useAxiosSecure();
   const [cardError, setCardError] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [processing, setProcessing] = useState(false);
+  const [transactionId, setTransactionId] = useState();
+
+  useEffect(() => {
+    axiosSecure.post("/create-payment-intent", { price }).then((res) => {
+      console.log(res.data.clientSecret);
+      setClientSecret(res.data.clientSecret);
+    });
+  }, [axiosSecure, price]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -27,10 +41,32 @@ const CheckoutForm = ({ selectedClass }) => {
     } else {
       setCardError("");
       console.log("payment method", paymentMethod);
-      console.log("selected class", selectedClass);
+      console.log("price", price, "id", _id);
+    }
+
+    setProcessing(true);
+
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log(confirmError);
+    }
+    console.log("paymentIntent", paymentIntent);
+    setProcessing(false);
+    if (paymentIntent.status === "succeeded") {
+      setTransactionId(paymentMethod.id);
+      //   const transactionId = paymentIntent.id;
     }
   };
-  console.log("selected class", selectedClass);
+  console.log("price", price, "id", _id);
 
   return (
     <div className="payment-card bg-gray-800 glass rounded-lg p-4">
@@ -57,11 +93,20 @@ const CheckoutForm = ({ selectedClass }) => {
               }}
             />
           </div>
-          <button type="submit" disabled={!stripe} className="btn   mt-4">
+          <button
+            type="submit"
+            disabled={!stripe || !clientSecret || processing}
+            className="btn   mt-4"
+          >
             Pay
           </button>
         </form>
         {cardError && <p className="text-red-500 ml-10 ">{cardError}</p>}
+        {transactionId && (
+          <p className="text-green-500">
+            Transaction Complete with transactionId: {transactionId}
+          </p>
+        )}
       </div>
     </div>
   );
