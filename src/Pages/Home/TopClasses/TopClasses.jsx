84f2aaa4a76c69ce223ec37dd5a0784f6d43e846
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-06-16 10:00:57 +0600

    top 6 classes sort by available seats done

--- src/Pages/Home/TopClasses/TopClasses.jsx
+++ src/Pages/Home/TopClasses/TopClasses.jsx
@@ -1,52 +1,68 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import required modules
-import { Pagination } from "swiper";
+const TopClasses = () => {
+  const [classes, setClasses] = useState([]);
 
-export default function TopClasses() {
+  useEffect(() => {
+    fetch("http://localhost:5000/top-classes")
+      .then((response) => response.json())
+      .then((data) => setClasses(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <>
+      <div className=" text-center font-extrabold text-3xl pt-10 lg:pt-20 pb-4">
+        Top Classes
+      </div>
       <Swiper
         slidesPerView={3}
         spaceBetween={30}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination]}
+        modules={[Autoplay, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-              <img
-                src="https://media.istockphoto.com/id/1153640359/photo/woman-in-kimono-performing-a-flying-kick-against-a-black-background.jpg?s=612x612&w=0&k=20&c=JIHC4TGeGGS361KHonRmN83YYuNDjDh-MywDWtvC42M="
-                alt="Shoes"
-                className="rounded-xl"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Shoes!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
+        {classes.map(({ _id, image, name, instructorName, availableSeats }) => (
+          <SwiperSlide key={_id}>
+            <div className="card w-96 bg-base-100 shadow-xl h-full flex flex-col">
+              <figure className="flex-grow-0 rounded-xl">
+                <img
+                  src={image}
+                  alt={name}
+                  className="rounded-xl p-4 w-full h-80 object-cover"
+                />
+              </figure>
+              <div className="card-body flex-grow flex flex-col items-center justify-center">
+                <h2 className="card-title">{name}</h2>
+                <p className="mb-2">Instructor: {instructorName}</p>
+                <p className="mb-4">Available Seats: {availableSeats}</p>
+                <div className="card-actions">
+                  <Link to="/classes">
+                    <button className="btn text-white btn-color">
+                      View Classes
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
-}
+};
+
+export default TopClasses;
